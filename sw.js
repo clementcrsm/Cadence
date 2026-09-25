7f8f79e18971bdc5f66e0f0ab605822a8cfab1c9
clementcrsm <kotio.crusem@gmail.com>
2026-09-25 14:06:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cadence : service worker minimal. Réseau d'abord, copie locale de l'app en secours hors connexion.
-const CACHE = 'cadence-v1.0.1';
+const CACHE = 'cadence-v1.0.2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
